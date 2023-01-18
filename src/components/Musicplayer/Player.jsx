9bff19a7dcdfc8fbbfb02cef09a44c4b82f1066e
jooedvard7 <jooedvard7@gmail.com>
2023-01-18 13:37:28 +0100

    api bug fix

--- src/components/Musicplayer/Player.jsx
+++ src/components/Musicplayer/Player.jsx
@@ -9,13 +9,8 @@ import {
 } from "react-icons/bi";
 import "animate.css";
 import music from "../../constant/music";
-import { useFetch } from "../../hooks/useFetch";
 
 const Player = ({ song, setSong }) => {
-  const API =
-    "https://yt-dl.prajjwalkapoor.repl.co/fetchAudio?videoURL=https://www.youtube.com/watch?v=";
-  const { isLoading, responseJSON, error } = useFetch(API + song.id);
-
   const [playing, setPlaying] = useState(false);
   const musicRef = useRef();
 
@@ -70,39 +65,27 @@ const Player = ({ song, setSong }) => {
       handleLoad();
       setPlaying(false);
     }
-  }, [responseJSON]);
+  }, [song]);
 
   return (
     <div className="section__padding">
       <div className="section__background">
         <img src={song.cover} alt={song.cover} className="animate__animated " />
       </div>
-      {responseJSON != null && !isLoading && (
+      {
         <div className="app__player_title">
-          {isLoading ? (
-            <div className="animate__animated animate__zoomOut">
-              {"Loading..."}
-            </div>
-          ) : (
-            <div className="animate__animated animate__zoomIn">
-              {song.title}
-            </div>
-          )}
+          <div className="animate__animated animate__zoomIn">{song.title}</div>
           <div
-            className={
-              isLoading
-                ? "app__player-symbol animate__animated animate__zoomOut"
-                : "app__player-symbol animate__animated animate__zoomIn"
-            }
+            className={"app__player-symbol animate__animated animate__zoomIn"}
           >
             <span className="app__player-dash"></span>
             <span className="app_player-dot"></span>
             <span className="app__player-dash"></span>
           </div>
         </div>
-      )}
+      }
 
-      {responseJSON != null && !isLoading && (
+      {(
         <div className="app__player_nav">
           <div className="app__player_buttons animate__animated animate__fadeInUp">
             <button onClick={handlePrevious}>
@@ -137,8 +120,8 @@ const Player = ({ song, setSong }) => {
           </div>
           <audio ref={musicRef}>
             <source
-              src={responseJSON[0].url}
-              type={responseJSON[0].mimeType}
+              src={song.url}
+              type={"audio/mp3"}
             ></source>
           </audio>
         </div>
--- src/constant/music.js
+++ src/constant/music.js
@@ -1,59 +1,79 @@
+import music1 from '../assests/music1.mp3';
+import morning_coffee from '../assests/morning_coffee.mp3';
+import chill_drive from '../assests/chill_drive.mp3';
+import midnight from '../assests/midnight.mp3';
+import autumn_chill from '../assests/autumn_chill.mp3';
+import vibe_ride from '../assests/vibe_ride.mp3';
+import smoke_japan from '../assests/smoke_japan.mp3';
+import oldschool from '../assests/oldschool.mp3';
+import hiphop_chill from '../assests/hiphop_chill.mp3';
+
 import { bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9 } from "./images"
+
 
 const music = [
     {
         title: "Slow Drive",
         id: "I3qutxxL8JU&t=3s",
         cover: bg1,
-        serial_number: 1
+        serial_number: 1,
+        url: music1
     },
     {
         title: "Morning Coffee",
         id: "7wVrD8Drkg0",
         cover: bg2,
-        serial_number: 2
+        serial_number: 2,
+        url: morning_coffee
     },
     {
         title: "Chill Drive",
         id: "AvnIQn-ChsM",
         cover: bg3,
-        serial_number: 3
+        serial_number: 3,
+        url: chill_drive
     },
     {
         title: "Midnight",
         id: "I8DxShZV9aU",
         cover: bg4,
-        serial_number: 4
+        serial_number: 4,
+        url: midnight
     },
     {
         title: "Autumn Chill",
         id: "6me17gGZYRg",
         cover: bg5,
-        serial_number: 5
+        serial_number: 5,
+        url: autumn_chill
     },
     {
         title: "Chill & Cruise",
         id: "TZZHa78ijwo",
         cover: bg6,
-        serial_number: 6
+        serial_number: 6,
+        url: vibe_ride
     },
     {
         title: "Smoking in Japan",
         id: "Qo0GR2qYMvQ",
         cover: bg7,
-        serial_number: 7
+        serial_number: 7,
+        url: smoke_japan
     },
     {
         title: "90s Hip Hop",
         id: "QDTpbB1uCiQ",
         cover: bg8,
-        serial_number: 8
+        serial_number: 8,
+        url: oldschool
     },
     {
         title: "Hip Hop Chill",
         id: "BtxcltY9sTA",
         cover: bg9,
-        serial_number: 9
+        serial_number: 9,
+        url: hiphop_chill
     }
 ]
 
